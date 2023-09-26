ae74fdc8b74a6d1bbb53df8936e68421f75d505f
Ariel Lima <114683489+ariel-lima@users.noreply.github.com>
2023-09-26 14:11:35 -0300

    Update para infos minhas

--- minimal.rb
+++ minimal.rb
@@ -38,8 +38,7 @@ gsub_file(
 # README
 ########################################
 markdown_file_content = <<~MARKDOWN
-  Rails app generated with [lewagon/rails-templates](https://github.com/lewagon/rails-templates), created by the [Le Wagon coding bootcamp](https://www.lewagon.com) team.
-MARKDOWN
+  Portfolio 
 file "README.md", markdown_file_content, force: true
 
 # Generators
@@ -117,5 +116,5 @@ after_bundle do
   ########################################
   git :init
   git add: "."
-  git commit: "-m 'Initial commit with minimal template from https://github.com/lewagon/rails-templates'"
+  git commit: "-m 'Initial commit with minimal template edited by myself"
 end
